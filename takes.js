ae45efcdde02829ca40931c83b4b0f5693393a3e
Creepy0964 <123pleasewerty@gmail.com>
2024-07-26 19:54:35 +0300

    Update from ASPD to XPD

--- takes.js
+++ takes.js
@@ -7,22 +7,22 @@ const bot = new TelegramBot(utils.utils.token_takes, {polling: true});
 const version = fs.readFileSync('version.txt', 'utf-8').slice(0, -1);
 
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, `Добро пожаловать в бота ASPD confession! Чтобы отправить тейк, напишите сюда его. К отправке принимаются: текст, видео, фото, аудио, голосовые сообщения.`);
+    bot.sendMessage(msg.chat.id, `Добро пожаловать в бота XPD Forum! Чтобы отправить тейк, напишите сюда его. К отправке принимаются: текст, видео, фото, аудио, голосовые сообщения.`);
 });
 
 bot.onText(/\/info/, (msg) => {
-    bot.sendMessage(msg.chat.id, `Чтобы отправить тейк, напишите текст и/или прикрепите медиа. Важно: если Вы хотите, чтобы ваш тейк был неанонимным, подпишите в конце текста тейка что-то по типу "неанон [ник]", к примеру: "неанон крипи".\n\nВерсия ботов: ${version}@master. По всем вопросам обращаться к @hopeeater`);
+    bot.sendMessage(msg.chat.id, `Чтобы отправить тейк, напишите текст и/или прикрепите медиа. Важно: если Вы хотите, чтобы ваш тейк был неанонимным, подпишите в конце текста тейка что-то по типу "неанон [ник]", к примеру: "неанон крипи".\n\nВерсия ботов: ${version}@master. По всем вопросам обращаться к @creepy0964`);
 });
 
 bot.on('message', (msg) => {
     if(msg.text == '/start' || msg.text == '/info') return;
     if(msg.chat.type != 'private') return;
     if(msg.text) {
-        bot.sendMessage(-1002249581112, `Новый тейк от ${msg.from.username != undefined ? `@${msg.from.username}` : `id: ${msg.from.id}`}: \n\n\`${msg.text}\n\n#тейк | @aspdcnf_takes_bot | @aspdcnf\``, {parse_mode: 'MarkdownV2'});
+        bot.sendMessage(-1002249581112, `Новый тейк от ${msg.from.username != undefined ? `@${msg.from.username}` : `id: ${msg.from.id}`}: \n\n\`${msg.text}\n\n#тейк | @xpdforum_takes_bot | @xpdforum\``, {parse_mode: 'MarkdownV2'});
     }
     else if(msg.audio || msg.video || msg.voice || msg.photo) {
         bot.sendMessage(-1002249581112, `Новый тейк от ${msg.from.username != undefined ? `@${msg.from.username}` : `id: ${msg.from.id}`}`);
-        bot.copyMessage(-1002249581112, msg.from.id, msg.message_id, {caption: `\`${msg.caption != undefined ? `${msg.caption}` : ``}\n\n#тейк | @aspdcnf_takes_bot | @aspdcnf\``, parse_mode: 'MarkdownV2'});
+        bot.copyMessage(-1002249581112, msg.from.id, msg.message_id, {caption: `\`${msg.caption != undefined ? `${msg.caption}` : ``}\n\n#тейк | @xpdforum_takes_bot | @xpdforum\``, parse_mode: 'MarkdownV2'});
     }
     else if(msg.sticker) {
         console.log('sticker, exiting...');
@@ -34,11 +34,11 @@ bot.on('message', (msg) => {
 bot.on('edited_message', (msg) => {
     if(msg.chat.type != 'private') return;
     if(msg.text) {
-        bot.sendMessage(-1002249581112, `Обновленный тейк от ${msg.from.username != undefined ? `@${msg.from.username}` : `id: ${msg.from.id}`}: \n\n\`${msg.text}\n\n#тейк | @aspdcnf_bot | @aspdcnf\``, {parse_mode: 'MarkdownV2'});
+        bot.sendMessage(-1002249581112, `Обновленный тейк от ${msg.from.username != undefined ? `@${msg.from.username}` : `id: ${msg.from.id}`}: \n\n\`${msg.text}\n\n#тейк | @xpdforum_takes_bot | @xpdforum\``, {parse_mode: 'MarkdownV2'});
     }
     else if(msg.audio || msg.video || msg.voice || msg.photo) {
         bot.sendMessage(-1002249581112, `Обновленный тейк от ${msg.from.username != undefined ? `@${msg.from.username}` : `id: ${msg.from.id}`}`);
-        bot.copyMessage(-1002249581112, msg.chat.id, msg.message_id, {caption: `\`${msg.caption != undefined ? `${msg.caption}` : ``}\n\n#тейк | @aspdcnf_bot | @aspdcnf\``, parse_mode: 'MarkdownV2'});
+        bot.copyMessage(-1002249581112, msg.chat.id, msg.message_id, {caption: `\`${msg.caption != undefined ? `${msg.caption}` : ``}\n\n#тейк | @xpdforum_takes_bot | @xpdforum\``, parse_mode: 'MarkdownV2'});
     }
     bot.sendMessage(msg.chat.id, `Изменения в тейк доставлены!`);
     console.log(`message updated from ${msg.chat.id} by ${msg.from.id}. text: ${msg.text}`);
